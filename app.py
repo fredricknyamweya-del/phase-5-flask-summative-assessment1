2f2b985c0bae2411ade339e1d29fb49b514107c8
fredricknyamweya-del <fredrick.nyamweya@student.moringaschool.com>
2026-07-02 15:36:18 +0300

    Addition of get method in routes to view all items and one item by id

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask, jsonify
+from data import inventory
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return jsonify({"message": "Welcome to the Inventory Management System API!"})
+    return jsonify({"message": "Welcome to the Inventory Management System API!",
+                    "endpoints": {
+                        "GET /inventory": "View all inventory items",
+                        "GET /inventory/<int:item_id>": "View a single inventory item by ID"
+                    }
+    })
+    
+#Get all items in the inventory
+@app.route('/inventory', methods=['GET'])
+def get_inventory():
+    return jsonify(inventory), 200
+
+#Get a single item in the inventory by ID
+@app.route('/inventory/<int:item_id>', methods=['GET'])
+def get_inventory_item(item_id):
+    item = next((item for item in inventory if item["id"] == item_id), None)
+    if item:
+        return jsonify(item), 200
+    else:
+        return jsonify({"error": "Item not found"}), 404
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
