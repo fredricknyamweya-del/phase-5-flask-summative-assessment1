30e279ba9cee519b4aec393d3bb1ba04c49d9621
fredricknyamweya-del <fredrick.nyamweya@student.moringaschool.com>
2026-07-02 19:22:03 +0300

    add CLI options to view a single item and add a new item, with input validation

--- cli.py
+++ cli.py
@@ -33,11 +33,38 @@ def view_one_item():
     print(f"Price: {item['price']}")
     print("--------------------\n")
 
+def add_item():
+    name = input("Enter item name: ")
+    quantity = input("Enter quantity: (integer) ")
+    price = input("Enter price: (float): ")
+    
+    try:
+        quantity = int(quantity)
+        price = float(price)
+    except ValueError:
+        print("Error: Quantity must be an integer and price must be a float.\n")
+        return
+
+    payload = {
+        "name": name,
+        "quantity": int(quantity),
+        "price": float(price)
+    }
+
+    response = requests.post(f"{BASE_URL}/inventory", json=payload)
+    if response.status_code == 201:
+        item = response.json()
+        print("\nItem added successfully:")
+        print(f"ID: {item['id']} | Name: {item['name']} | Quantity: {item['quantity']} | Price: {item['price']}\n")
+    else:
+        print("Error: Could not add item.\n")
+
 def main_menu():
     while True:
         print("=== Inventory Management CLI ===")
         print("1. View all items")
         print("2. View one item by ID")
+        print("3. Add new item")
         print("0. Exit")
         choice = input("Enter your choice: ")
 
@@ -45,6 +72,8 @@ def main_menu():
             view_all_items()
         elif choice == "2":
             view_one_item()
+        elif choice == "3":
+            add_item()
         elif choice == "0":
             print("Goodbye!")
             break
